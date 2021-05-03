f2909e55fa733ec4d4ee3b45e188e4bdc5fbf616
Baran Pasa <34427951+BaranPasa451@users.noreply.github.com>
2021-05-02 22:25:21 -0400

    first real commit
    Added the beginning of the board, finish.

---
+++ main.py
@@ -0,0 +1,13 @@
+import random
+
+board = {
+    "a1": None, "a2": None, "a3": None, "a4": None, "a5": None, "a6": None, "a7": None, "a8": None, "a9": None,
+    "b1": None, "b2": None, "b3": None, "b4": None, "b5": None, "b6": None, "b7": None, "b8": None, "b9": None,
+    "c1": None, "c2": None, "c3": None, "c4": None, "c5": None, "c6": None, "c7": None, "c8": None, "c9": None,
+    "d1": None, "d2": None, "d3": None, "d4": None, "d5": None, "d6": None, "d7": None, "d8": None, "d9": None,
+    "e1": None, "e2": None, "e3": None, "e4": None, "e5": None, "e6": None, "e7": None, "e8": None, "e9": None,
+    "f1": None, "f2": None, "a3": None, "a4": None, "a5": None, "a6": None, "a7": None, "a8": None, "a9": None,
+    "g1": None, "g2": None, "a3": None, "a4": None, "a5": None, "a6": None, "a7": None, "a8": None, "a9": None,
+    "h1": None, "h2": None, "a3": None, "a4": None, "a5": None, "a6": None, "a7": None, "a8": None, "a9": None,
+    "i1": None, "i2": None, "a3": None, "a4": None, "a5": None, "a6": None, "a7": None, "a8": None, "a9": None,
+}
